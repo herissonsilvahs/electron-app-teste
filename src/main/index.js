11869009b3d63a4625b7e8e58dbfaa7cf621fcae
Herisson Silva <herissonsilvahs@gmail.com>
2018-11-07 16:12:20 -0300

    autoUpdate

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,28 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, autoUpdater } from 'electron'
 
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
+
+const server = `https://codeload.github.com/herissonsilvahs/electron-app-teste/zip/${app.getVersion()}`
+
+autoUpdater.setFeedURL(server)
+
+autoUpdater.checkForUpdates()
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  console.log(dialogOpts)
+})
 
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
